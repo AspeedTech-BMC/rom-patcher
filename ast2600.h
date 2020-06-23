fdd8716eab86313f4681528eddbf8432f145dd03
Dylan <Dylan@DylantekiMacBook-Air.local>
2020-06-23 14:16:05 +0800

    add debug print

--- ast2600.h
+++ ast2600.h
@@ -3,26 +3,47 @@
 
 #include "config.h"
 
-#define SCU_BASE 			0x1e6e2000
-#define MPLL_REG 			(SCU_BASE + 0x220)
-#define MPLL_EXT_REG 		(SCU_BASE + 0x224)
-#define STRAP_REG			(SCU_BASE + 0x500)
+#define SCU_BASE 						0x1e6e2000
+#define MPLL_REG 						(SCU_BASE + 0x220)
+#define MPLL_EXT_REG 					(SCU_BASE + 0x224)
+#define STRAP_REG						(SCU_BASE + 0x500)
 
-#define MPLL_FREQ_400M 		0x0008405f
-#define MPLL_EXT_400M 		0x00000031
+#define MPLL_FREQ_400M 					0x0008405f
+#define MPLL_EXT_400M 					0x00000031
 
-#define MMC_BASE 			0x1e6e0000
-#define PHY_BASE 			(MMC_BASE + 0x100)
-#define PHY_STS_BASE		(MMC_BASE + 0x400)
-#define MMC_UNLOCK_KEY 		0xfc600309
+#define MMC_BASE 						0x1e6e0000
+#define PHY_BASE 						(MMC_BASE + 0x100)
+#define PHY_STS_BASE					(MMC_BASE + 0x400)
+#define MMC_UNLOCK_KEY 					0xfc600309
+
+#define UART_BASE						0x1e784000
+/* DLAB = 0 */
+#define UART_THR                        0x00
+#define UART_RBR                        0x00
+#define UART_IER                        0x04
+/* DLAB = 1 */
+#define UART_DLL                        0x00
+#define UART_DLH                        0x04
+
+#define UART_IIR                        0x08    /* for read */
+#define UART_FCR                        0x08    /* for write */
+#define UART_LCR                        0x0C
+#define UART_MCR                        0x10
+#define UART_LSR                        0x14
+#define UART_MSR                        0x18
+#define UART_SCR                        0x1C
+
+/* bitfields for UART_LSR */
+#define UART_LSR_THRE                   BIT(5)
+#define UART_LSR_DR                     BIT(0)
 
 /* secure boot controller */
-#define SBC_BASE			0x1e6f2000
-#define OTP_QSR				0x40
+#define SBC_BASE						0x1e6f2000
+#define OTP_QSR							0x40
 
-#define SRAM_BASE			0x10000000	/* 64KB */
-#define SRAM1_BASE			0x10010000	/* 24KB */
-#define SPI_BASE			0x20000000
-#define DRAM_BASE			CONFIG_SYS_SDRAM_BASE
+#define SRAM_BASE						0x10000000	/* 64KB */
+#define SRAM1_BASE						0x10010000	/* 24KB */
+#define SPI_BASE						0x20000000
+#define DRAM_BASE						CONFIG_SYS_SDRAM_BASE
 
 #endif /* end of "ifndef _AST2600_H_" */
--- main.c
+++ main.c
@@ -7,6 +7,25 @@
 #include "ddr4_init.h"
 #include "bootloader.h"
 
+void uart_init(FILE *fp)
+{
+	wr_single(fp, UART_BASE + UART_LCR, 0x3);
+	wr_single(fp, UART_BASE + UART_FCR, 0xc7);
+	wr_single(fp, UART_BASE + UART_LCR, 0x83);
+
+	wr_single(fp, UART_BASE + UART_DLL, 0x1);
+	wr_single(fp, UART_BASE + UART_DLH, 0x0);
+
+	wr_single(fp, UART_BASE + UART_LCR, 0x3);
+
+	wr_single(fp, UART_BASE + UART_THR, 0xd);
+	wr_single(fp, UART_BASE + UART_THR, 0xa);
+}
+
+void uart_putc(FILE *fp, uint8_t c)
+{
+	wr_single(fp, UART_BASE + UART_THR, c);
+}
 /**
  *                 0000_0000 +--------------------+
  *                           | ARM CA7 code
@@ -46,6 +65,8 @@ int main()
 
     /* ---------- start ---------- */ 
 	start_code(fp);
+	uart_init(fp);
+	uart_putc(fp, '1');
 	jmp_code(fp, "l_start");
 	fgetpos(fp, &cm3_img_start);
 	attach_cm3_binary(fp);
@@ -53,7 +74,7 @@ int main()
 	declare_label(fp, "l_start");
 	/* goto l_calc_size if DRAM is already initialized */
     jeq_code(fp, SCU_BASE + 0x100, BIT(6), BIT(6), "l_calc_size");
-
+	uart_putc(fp, '2');
 	/* set MPLL */
     rmw_code(fp, MPLL_REG, ~(BIT(24) | GENMASK(22, 0)),
 	     BIT(25) | BIT(23) | MPLL_FREQ_400M);
@@ -61,7 +82,7 @@ int main()
     delay_code(fp, 100);
     rmw_code(fp, MPLL_REG, GENMASK(31, 0), ~(BIT(25) | BIT(23)));
     waiteq_code(fp, MPLL_EXT_REG, BIT(31), BIT(31), 1);
-
+	uart_putc(fp, '3');
 	/* ast2600_sdrammc_unlock */
     wr_single(fp, MMC_BASE, MMC_UNLOCK_KEY);
     waiteq_code(fp, MMC_BASE, BIT(0), BIT(0), 1);
@@ -77,16 +98,18 @@ int main()
 	waiteq_code(fp, PHY_BASE + 0x300, BIT(1), BIT(1), 1);
 	sdramphy_check_status(fp);
 #endif
+	uart_putc(fp, '4');
 
     declare_label(fp, "l_calc_size");
 	sdrammc_calc_size(fp);
 
     /* DDR4 init end: set handshake bits */
     setbit_code(fp, SCU_BASE + 0x100, BIT(7) | BIT(6));
-
+	uart_putc(fp, '5');
 	/* download and enable CM3 */
 	copy_cm3(fp, cm3_img_start);
 	enable_cm3(fp);
+	uart_putc(fp, '6');
     quit_code(fp);
 
 	print_labels();
